d974f17ce9fd3dbc57d1e46f036d2638af60dfa4
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-15 15:54:55 +0300

    Добавьте файлы проекта.

---
+++ Занятие 17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Занятие_17
+{ // Класс для моделирования счета в банке
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        }
+    }
+    class BankAccount <T>
+    {
+        private int account;
+        private double balance;
+        private string name;
+
+
+    }
+}
