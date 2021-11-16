8919328a78d1e24da052be915a81c9db4323fb87
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-16 13:21:23 +0300

    Задача

--- Занятие 17/Program.cs
+++ Занятие 17/Program.cs
@@ -10,21 +10,65 @@ namespace Занятие_17
     {
         static void Main(string[] args)
         {
-            BankAccount<int> account = new BankAccount<int>();
-            Console.WriteLine("Ввести номер счёта:");
-            BankAccount<int> balance = new BankAccount<int>();
-            Console.WriteLine("Ввести баланс счёта:");
-            BankAccount<string> name = new BankAccount<string>();
-            Console.WriteLine("Ввести  фио владельца счёта:");
+            BankAccount<int>bankAccount1 = new BankAccount<int>();
+
+            BankAccount<double> bankAccount2 = new BankAccount<double>();
+
+            BankAccount<string> bankAccount3 = new BankAccount<string>();
+            
+            Console.ReadKey();
 
         }
     }
-    class BankAccount <T>  // Класс для моделирования счета в банке с универсальным параметром
+    class BankAccount<T>  // Класс для моделирования счета в банке с универсальным параметром
     {
-        internal int account;    //закрытые поля для номера счета
-        internal double balance; // ...баланса
-        internal string name;    //фио владельца
+        private T account;    //закрытые поля для номера счета
+        private double balance; // ...баланса
+        private string name;    //фио владельца
+        public void Info<T>(T value)
+        {
+            Console.WriteLine();
+        }
+        public T Account
+        {
+            set
+            {
+                account = value;
+            }
+            get
+            {
+                return account;
+            }
+        }
+        public double Balance
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Баланс счета не может быть отрицательным");
+                }
+            }
+            get
+            {
+                return balance;
+            }
+        }
 
-
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
     }
 }
