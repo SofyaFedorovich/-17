cb7033b054f4d5b2264ba68a21f829495cf9b703
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-16 11:15:01 +0300

    Задача

--- Занятие 17/Program.cs
+++ Занятие 17/Program.cs
@@ -5,18 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Занятие_17
-{ // Класс для моделирования счета в банке
+{
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            BankAccount<int> account = new BankAccount<int>();
+            Console.WriteLine("Ввести номер счёта:");
+            BankAccount<int> balance = new BankAccount<int>();
+            Console.WriteLine("Ввести баланс счёта:");
+            BankAccount<string> name = new BankAccount<string>();
+            Console.WriteLine("Ввести  фио владельца счёта:");
+
         }
     }
-    class BankAccount <T>
+    class BankAccount <T>  // Класс для моделирования счета в банке с универсальным параметром
     {
-        private int account;
-        private double balance;
-        private string name;
+        internal int account;    //закрытые поля для номера счета
+        internal double balance; // ...баланса
+        internal string name;    //фио владельца
 
 
     }
